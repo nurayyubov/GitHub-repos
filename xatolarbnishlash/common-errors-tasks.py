81bf787bd07f293c73ff0759d6e7b92709d3e1f5
nurayyubov <ayyubov.hallo@gmail.com>
2024-12-03 09:28:13 +0900

    new tasks done

---
+++ xatolarbnishlash/common-errors-tasks.py
@@ -0,0 +1,96 @@
+#xatolar bilan ishlash
+
+# 1-mashq
+
+# son = int(input("Juft son kiriting: "))
+# if son%2!=0:
+#     print("Bu son juft emas.")
+# else:
+#     print("Rahmat!")
+    
+# 2-mashq
+
+# yosh = int(input("Yoshingiz nechida?"))
+
+# if yosh<=4 or yosh>=60:
+#     narh = 0
+# elif yosh < 18:
+#     narh = 10000
+# else:
+#     narh = 20000
+
+# print(f"Chipta {narh} so'm")
+
+#3-mashq
+
+# x = int(input("Birinchi sonni kiriting: "))
+# y = int(input("Ikkinchi sonni kiriting: "))
+
+# if x==y:
+#     print(f"{x}={y}")
+# elif x<y:
+#     print(f"{x}<{y}")
+# else:
+#     print(f"{x}>{y}")
+
+# 4-mashq
+
+# mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+#                'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+
+# savat= []
+# for n in range(5):
+#     savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
+
+# if savat:
+#     for mahsulot in savat:
+#         if mahsulot in mahsulotlar:
+#             print(f"Do'konimizda {mahsulot} bor")
+#         else:
+#             print(f"Do'konimizda {mahsulot} yo'q")
+# else: 
+#     print("Savatingiz bo'sh")            
+
+# 5-mashq
+
+# mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+#                'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+
+# savat = []
+# for n in range(5):
+#     savat.append(input(f'Savatga {n+1}-mahsulotni qo\'shing: '))
+
+# bor_mahsulotlar = []
+# mavjud_emas = []
+# for mahsulot in savat:
+#     if mahsulot in mahsulotlar:
+#         bor_mahsulotlar.append(mahsulot)
+#     else:
+#         mavjud_emas.append(mahsulot)
+
+# if mavjud_emas:
+#     print("Do'konimizda quyidagi mahsulotlar yo'q:")
+#     for mahsulot in mavjud_emas:
+#         print(mahsulot)
+        
+#     print("mavjud mahsulotlar: ")
+#     for mahsulot in bor_mahsulotlar:
+#         print(mahsulot)
+# else:
+#   print("Siz so'ragan boshqa mahsulotlar do'konimizda bor")
+
+# 6-mashq
+
+# users = ['alisher1983','aziza','yasina' 'umar']
+
+# login = input("Yangi login tanlang: ")
+
+# if login in users:
+#     print('Login band, yangi login tanalng!')
+# else:
+#     print("Xush kelibsiz!")
+  
+  
+  
+  
+    
